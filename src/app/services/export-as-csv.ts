f97b36656aa05cf7ff162bde2f15f5fbf79a62a4
vicnent agi <vicnent@inviseo.fr>
2023-08-22 10:21:25 +0200

    .

---
+++ src/app/services/export-as-csv.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { saveAs } from 'file-saver';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ExportDataAsCsvService {
+
+  private saveAsFile(buffer: any, fileName: string, fileType: string): void {
+    const data: Blob = new Blob([buffer], { type: fileType });
+    saveAs(data, fileName);
+  }
+
+  public exportToCsv(rows: object[], fileName: string, columns?: string[]): any {
+    if (!rows || !rows.length) {
+      return;
+    }
+    const separator = ',';
+    const keys = Object.keys(rows[0]).filter(k => {
+      if (columns?.length) {
+        return columns.includes(k);
+      } else {
+        return true;
+      }
+    });
+    const csvContent =
+      keys.join(separator) +
+      '\n' +
+      rows.map((row:any) => {
+        return keys.map(k => {
+          let cell = row[k] === null || row[k] === undefined ? '' : row[k];
+          cell = cell instanceof Date
+            ? cell.toLocaleString()
+            : cell.toString().replace(/"/g, '""');
+          if (cell.search(/("|,|\n)/g) >= 0) {
+            cell = `"${cell}"`;
+          }
+          return cell;
+        }).join(separator);
+      }).join('\n');
+    this.saveAsFile(csvContent, `${fileName}`, '.csv');
+  }
+
+  private aggregate(datas: any, aggregateOption: number): any {
+      return datas
+  }
+}
